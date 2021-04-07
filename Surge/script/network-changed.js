fd436029763a7473cfa2b42f2f4554dcd198932b
PanLiu <vitoegg@gmail.com>
2021-04-07 17:13:40 +0800

    Update network-changed.js

--- Surge/script/network-changed.js
+++ Surge/script/network-changed.js
@@ -1,45 +1,32 @@
-//event network-changed script-path=network-changed.js
-//version: 2.2
-//auther: tempoblink
-//引用自: https://github.com/Tempoblink/Surge-Scripts/blob/master/network-changed.js
-
-//The Notification Format.
-//The Notification Format.
-let TITLE = 'Outbound Changed!';
-let SUBTITLE_CELLULAR = 'NetWork: ';
-let SUBTITLE_WIFI = 'Wi-Fi: ';
-let ABOUT_MODE = 'Outbound mode: ';
-let ABOUT_IP = 'New IP address: ';
-
-//Home ssid.
-
-let PROXYWIFI = [
-            "Tech",
-            "MyWifi"
-    ];
-
-//The default outbound: 'Direct' or 'Rule' or 'Global-proxy'.
-let DirectMode = "Direct";
-let RuleMode = "Rule";
-
-function changeOutboundMode(mode) {
-    ABOUT_IP += $network.v4.primaryAddress;
-    if($surge.setOutboundMode(mode.toLowerCase()))
-        $notification.post(TITLE, NETWORK, ABOUT_MODE + mode + '\n' + ABOUT_IP);
+/**
+ * @description
+ * 如果是家里WI-FI则开启直连模式
+ * 如果不是家里WI-FI则开启代理模式
+ */
+const WIFI_DONT_NEED_PROXYS = ['Tech','MyWifi'];
+if (wifiChanged()) {
+  if (WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)) {
+    $surge.setOutboundMode('direct');
+    $notification.post(
+      'Surge',
+      `Wi-Fi changed to ${$network.wifi.ssid}`,
+      'use direct mode'
+    );
+  } else {
+    $surge.setSelectGroupPolicy('Final-select', 'Group');
+    $surge.setOutboundMode('rule');
+    $notification.post(
+      'Surge',
+      `Wi-Fi changed to ${$network.wifi.ssid}`,
+      'use rule-based proxy mode'
+    );
+  }
 }
-
-//wifi select outbound
-let NETWORK = "";
-if ($network.v4.primaryInterface == "en0") {
-    NETWORK += SUBTITLE_WIFI + $network.wifi.ssid;
-    if (PROXYWIFI.indexOf($network.wifi.ssid) != -1) {
-        changeOutboundMode(DirectMode);
-    } else {
-        changeOutboundMode(RuleMode);
-    }
-}else {
-    NETWORK += SUBTITLE_CELLULAR + Cellular-Data;
-    changeOutboundMode(RuleMode);
+function wifiChanged() {
+  const currentWifiSSid = $persistentStore.read('current_wifi_ssid');
+  const changed = currentWifiSSid !== $network.wifi.ssid;
+  if (changed) {
+    $persistentStore.write($network.wifi.ssid, 'current_wifi_ssid');
+  }
+  return changed;
 }
-
-$done();
