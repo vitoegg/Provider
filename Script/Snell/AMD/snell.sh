412fe65ca7783bc75506edb3b255301246991fc7
PanLiu <vitoegg@gmail.com>
2021-09-15 09:49:43 +0800

    Update snell.sh

--- Script/Snell/AMD/snell.sh
+++ Script/Snell/AMD/snell.sh
@@ -5,7 +5,7 @@ CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
 apt-get install unzip -y
 cd ~/
-wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/2.0.5b3/snell-server-v2.0.5-linux-amd64.zip
+wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/v2.0.4/snell-server-v2.0.4-linux-amd64.zip
 unzip -o snell.zip
 rm -f snell.zip
 chmod +x snell-server
@@ -22,9 +22,9 @@ if [ -f ${CONF} ]; then
   mkdir /etc/snell/
   echo "Generating new config..."
   echo "[snell-server]" >>${CONF}
-  echo "listen = 0.0.0.0:59188" >>${CONF}
+  echo "listen = 0.0.0.0:59187" >>${CONF}
   echo "psk = ${PSK}" >>${CONF}
-  echo "obfs = http" >>${CONF}
+  echo "obfs = tls" >>${CONF}
 fi
 if [ -f ${SYSTEMD} ]; then
   echo "Found existing service..."
