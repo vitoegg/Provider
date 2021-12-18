236a2a1c9e0b31d0ae6f664c58939754c174fea0
PanLiu <vitoegg@gmail.com>
2021-12-18 19:07:26 +0800

    Update snell.sh

--- Script/Snell/ARM/snell.sh
+++ Script/Snell/ARM/snell.sh
@@ -5,7 +5,7 @@ CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
 apt-get install unzip -y
 cd ~/
-wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/2.0.5b3/snell-server-v2.0.5-linux-aarch64.zip
+wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/v2.1.0b1/snell-server-v2.1.0-linux-aarch64.zip
 unzip -o snell.zip
 rm -f snell.zip
 chmod +x snell-server
