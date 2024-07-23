91fb4f7acfae2bf28242c15d6b236efd7f38c841
Liu Pan <vitoegg@gmail.com>
2024-07-23 13:59:22 +0800

    Change Default Port

--- Script/Snell/AMD/snell.sh
+++ Script/Snell/AMD/snell.sh
@@ -22,7 +22,7 @@ if [ -f ${CONF} ]; then
   mkdir /etc/snell/
   echo "Generating new config..."
   echo "[snell-server]" >>${CONF}
-  echo "listen = 0.0.0.0:59187" >>${CONF}
+  echo "listen = 0.0.0.0:59168" >>${CONF}
   echo "psk = ${PSK}" >>${CONF}
   echo "ipv6 = false" >>${CONF}
 fi
