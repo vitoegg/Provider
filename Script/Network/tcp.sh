1a585067b46cfc53f6fda6e6e5abb71358bbee1e
Liu Pan <vitoegg@gmail.com>
2024-11-01 18:42:54 +0800

    Update tcp.sh

--- Script/Network/tcp.sh
+++ Script/Network/tcp.sh
@@ -38,12 +38,8 @@ net.ipv4.tcp_window_scaling=1
 net.ipv4.tcp_adv_win_scale=1
 net.ipv4.tcp_moderate_rcvbuf=1
 # 网络缓存区调整
-net.core.rmem_max=33554432
-net.core.wmem_max=33554432
-net.ipv4.tcp_rmem=4096 87380 33554432
-net.ipv4.tcp_wmem=4096 16384 33554432
-net.ipv4.udp_rmem_min=8192
-net.ipv4.udp_wmem_min=8192
+net.ipv4.tcp_rmem=8192 26214 13421773
+net.ipv4.tcp_wmem=4096 16384 6710886
 # 禁用IPv6
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
@@ -53,53 +49,9 @@ EOF
     sysctl -p && sysctl --system
 }
 
-ulimit_tune() {
-    ulimit -SHn 1000000 && ulimit -c unlimited
-    cat > /etc/security/limits.conf << EOF
-root     soft   nofile    1000000
-root     hard   nofile    1000000
-root     soft   nproc     1000000
-root     hard   nproc     1000000
-root     soft   core      1000000
-root     hard   core      1000000
-root     hard   memlock   unlimited
-root     soft   memlock   unlimited
-
-*     soft   nofile    1000000
-*     hard   nofile    1000000
-*     soft   nproc     1000000
-*     hard   nproc     1000000
-*     soft   core      1000000
-*     hard   core      1000000
-*     hard   memlock   unlimited
-*     soft   memlock   unlimited
-EOF
-
-    if ! grep -q "ulimit" /etc/profile; then
-        echo "ulimit -SHn 1000000" >> /etc/profile
-    fi
-    if ! grep -q "pam_limits.so" /etc/pam.d/common-session; then
-        echo "session required pam_limits.so" >> /etc/pam.d/common-session
-    fi
-
-    cat > /etc/systemd/system.conf << EOF
-[Manager]
-DefaultTimeoutStopSec=30s
-DefaultLimitCORE=infinity
-DefaultLimitNOFILE=65535
-DefaultLimitNPROC=65535
-EOF
-
-    systemctl daemon-reload
-}
-
 echo -e "${Info} 开始执行系统调优..."
 system_tune
 echo -e "${Info} 系统调优完成"
-
-echo -e "${Info} 开始执行系统资源限制调优..."
-ulimit_tune
-echo -e "${Info} 系统资源限制调优完成"
 
 echo -e "${Tip} 部分设置可能需要重启系统才能生效"
 echo -e "${Tip} 已禁用IPv6，如果需要使用IPv6，请手动修改 /etc/sysctl.conf 文件"
