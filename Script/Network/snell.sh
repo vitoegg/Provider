9a9715f66a9f7dff8c52d20199517789fe75eeac
Liu Pan <vitoegg@gmail.com>
2024-11-04 20:01:04 +0800

    Update snell.sh

--- Script/Network/snell.sh
+++ Script/Network/snell.sh
@@ -119,6 +119,8 @@ Wants=network-online.target systemd-networkd-wait-online.service
 
 [Service]
 Type=simple
+User=nobody
+LimitNOFILE=65536
 ExecStart=/usr/local/bin/snell-server -c ${CONF}
 Restart=on-failure
 RestartSec=5s
