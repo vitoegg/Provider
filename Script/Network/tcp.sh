ae1facec2bef46b2e0aabb21cde2f59736b2bfb9
Liu Pan <vitoegg@gmail.com>
2025-02-14 19:09:44 +0800

    Update tcp.sh

--- Script/Network/tcp.sh
+++ Script/Network/tcp.sh
@@ -94,8 +94,12 @@ simple_bbr_tune() {
 net.core.default_qdisc = cake
 net.ipv4.tcp_congestion_control = bbr
 # Network buffer tuning
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
 net.ipv4.tcp_rmem=4096 87380 ${Rmem}
 net.ipv4.tcp_wmem=4096 16384 ${Wmem}
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
 # Enable packet forwarding
 net.ipv4.ip_forward=1
 net.ipv4.conf.all.route_localnet=1
@@ -142,8 +146,12 @@ net.ipv4.tcp_window_scaling=1
 net.ipv4.tcp_adv_win_scale=1
 net.ipv4.tcp_moderate_rcvbuf=1
 # Network buffer tuning
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
 net.ipv4.tcp_rmem=4096 87380 ${Rmem}
 net.ipv4.tcp_wmem=4096 16384 ${Wmem}
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
 # Enable packet forwarding
 net.ipv4.ip_forward=1
 net.ipv4.conf.all.route_localnet=1
