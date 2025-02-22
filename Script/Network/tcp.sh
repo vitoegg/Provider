140567783071973c4f4bb44a258e4a2bba28f8a8
Liu Pan <vitoegg@gmail.com>
2025-02-22 11:32:18 +0800

    Update tcp.sh

--- Script/Network/tcp.sh
+++ Script/Network/tcp.sh
@@ -35,15 +35,15 @@ server_selection() {
     read -p "Enter your choice (1-4): " server_choice
     case $server_choice in
         1)
-            Rmem=6875000
+            Rmem=9699328
             Wmem=6875000
             ;;
         2)
-            Rmem=7471104
+            Rmem=9699328
             Wmem=7471104
             ;;
         3)
-            Rmem=18750000
+            Rmem=22750000
             Wmem=18750000
             ;;
         4)
@@ -64,18 +64,31 @@ server_selection() {
 ################################################################################
 bbr_tcp_tune() {
     cat > /etc/sysctl.conf << EOF
+# Memory recovery and swap control
+vm.swappiness = 10
+vm.min_free_kbytes = 153600
 # TCP congestion control
-net.core.default_qdisc = fq
+net.core.default_qdisc = cake
 net.ipv4.tcp_congestion_control = bbr
 # Network buffer tuning
-net.ipv4.tcp_rmem=8192 87380 ${Rmem}
-net.ipv4.tcp_wmem=8192 65536 ${Wmem}
+net.ipv4.tcp_rmem=8192 131072 ${Rmem}
+net.ipv4.tcp_wmem=8192 98304 ${Wmem}
 # Increase queue length
-net.core.somaxconn = 4096
+net.core.netdev_max_backlog = 4000
+net.core.somaxconn = 1024
 # Enable window expansion
 net.ipv4.tcp_window_scaling=1
-net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_adv_win_scale=2
 net.ipv4.tcp_moderate_rcvbuf=1
+# ARP and Neighbor Configuration
+net.ipv4.neigh.default.gc_stale_time = 120
+net.ipv4.neigh.default.gc_thresh1 = 1024
+net.ipv4.neigh.default.gc_thresh2 = 4096
+net.ipv4.neigh.default.gc_thresh3 = 8192
+net.ipv4.conf.all.arp_announce = 2
+net.ipv4.conf.default.arp_announce = 2
+net.ipv4.conf.all.arp_ignore = 1
+net.ipv4.conf.default.arp_ignore = 1
 # Enable packet forwarding
 net.ipv4.ip_forward=1
 net.ipv4.conf.all.route_localnet=1
